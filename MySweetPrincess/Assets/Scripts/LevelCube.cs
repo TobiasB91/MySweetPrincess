6913eb1adcb94cd2a719882ef866110292e26edd
elbingobonito <tobi.brandt@gmx.net>
2016-06-06 22:51:04 +0200

    LevelCube and FanCube added

---
+++ MySweetPrincess/Assets/Scripts/LevelCube.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelCube : MonoBehaviour {
+
+	public Texture2D floorTexture;
+
+	// Use this for initialization
+	protected virtual void Start () {
+		gameObject.GetComponent<Renderer>().material.mainTexture = floorTexture;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
