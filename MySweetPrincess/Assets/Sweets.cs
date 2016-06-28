05add05fa8463fe58bff0874b16b94e5596e5cd0
waldemar49 <waldi.wegele@gmx.de>
2016-06-28 13:04:34 +0200

    Level built

--- MySweetPrincess/Assets/Sweets.cs
+++ MySweetPrincess/Assets/Sweets.cs
@@ -4,4 +4,11 @@ using System.Collections;
 public class Sweets : MonoBehaviour {
 
     public int calories;
+    public float rotSpeed;
+
+    void Update() {
+        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x,
+                                                          transform.rotation.eulerAngles.y + rotSpeed,
+                                                          transform.rotation.eulerAngles.z));
+    }
 }
